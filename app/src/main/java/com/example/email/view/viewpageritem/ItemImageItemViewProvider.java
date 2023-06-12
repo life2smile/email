0547ef0bc531310b76d7bf5621327ffd1a9b74f4
hanxiao <liuji.llj@alibaba-inc.com>
2023-06-12 14:40:53 +0800

    feat:增加glide header

--- app/src/main/java/com/example/email/view/viewpageritem/ItemImageItemViewProvider.java
+++ app/src/main/java/com/example/email/view/viewpageritem/ItemImageItemViewProvider.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.load.model.LazyHeaders;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.email.R;
@@ -25,6 +27,12 @@ public class ItemImageItemViewProvider implements IItemViewProvider {
         ImageView bannerIv = view.findViewById(R.id.iv_banner_item);
         RequestOptions options = RequestOptions
                 .bitmapTransform(new RoundedCorners(Util.dp2Px(view.getContext(), 24)));
-        Glide.with(view).load(homeData.getImgUrl()).apply(options).into(bannerIv);
+
+        GlideUrl glideUrl = new GlideUrl(homeData.getImgUrl(), new LazyHeaders.Builder()
+                .addHeader("User-Agent",
+                        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit / 537.36(KHTML, like Gecko) Chrome  47.0.2526.106 Safari / 537.36")
+                .build());
+        Glide.with(view)
+                .load(glideUrl).apply(options).into(bannerIv);
     }
 }
